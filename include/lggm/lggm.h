baf6bd73008c0442d7fece6d98a9fa3ba5c6c20e
boil <boil@localhost.localdomain>
2019-05-05 22:43:37 +0300

    added sreaming

--- include/lggm/lggm.h
+++ include/lggm/lggm.h
@@ -1,17 +1,19 @@
 #pragma once
 
 #include <cstdint>
+#include <ostream>
 #include <fstream>
 #include <string>
 #include <chrono>
 #include <iomanip>
+#include <type_traits>
 
 namespace lggm
 {
 
 std::string const g_outFileName = "/home/boil/Work/MyTmp/lggm.txt";
 
-enum /*class*/ format : uint8_t
+enum class format : uint8_t
 {
   none,
   timestamp = 0b1,
@@ -20,13 +22,16 @@ enum /*class*/ format : uint8_t
   line = 0b1000,
 };
 
-constexpr uint8_t g_defaultFormat = format::timestamp | format::function | format::line;
+constexpr uint8_t g_defaultFormat = static_cast<std::underlying_type<format>::type> ( format::timestamp )
+                                    | static_cast<std::underlying_type<format>::type> ( format::function )
+                                    | static_cast<std::underlying_type<format>::type> ( format::line );
 
 class lggm
 {
 public:
-  lggm ( size_t lineNo, std::string const& functName, std::string const& msg )
-    : m_ofs ( m_outFileName, std::ios::out | std::ios::app )
+  lggm () : m_ofs ( m_outFileName, std::ios::out | std::ios::app ) { /* EMPTY */ }
+
+  void doMessage ( size_t lineNo, std::string const& functName, std::string const& msg )
   {
     if ( !m_ofs.is_open() )
     {
@@ -34,12 +39,10 @@ public:
     }
 
     m_ofs << printTimestamp() << printDisposition ( lineNo, functName ) << msg << std::endl;
-
   }
 
   template <typename T>
-  lggm ( size_t lineNo, std::string const& functName, std::string const& valueName, T value )
-    : m_ofs ( m_outFileName, std::ios::out | std::ios::app )
+  void doNameValue ( size_t lineNo, std::string const& functName, std::string const& valueName, T value )
   {
     if ( !m_ofs.is_open() )
     {
@@ -48,14 +51,13 @@ public:
 
     m_ofs << printTimestamp() << printDisposition ( lineNo,
           functName ) << "\"" << valueName << "\" = '"  << value  << "'" << std::endl;
-
   }
 
-  lggm ( size_t lineNo, std::string const& functName )
-    : m_ofs ( m_outFileName, std::ios::out | std::ios::app )
-    , m_lineNo ( lineNo )
-    , m_functName ( functName )
+  void  doScope ( size_t lineNo, std::string const& functName )
   {
+    m_lineNo = lineNo;
+    m_functName = functName;
+
     if ( !m_ofs.is_open() )
     {
       return;
@@ -64,25 +66,46 @@ public:
     outputScopedMessage ( "->" );
   }
 
+  std::ostream& doStream ( size_t lineNo, std::string const& functName )
+  {
+    m_isStreamed = true;
+
+    m_ofs << printTimestamp() << printDisposition ( lineNo, functName );
+
+    return getOutStream();
+  }
+
   ~lggm()
   {
-    if ( !m_isScoped )
+    if ( m_isScoped )
     {
-      return;
-    }
+      if ( !m_ofs.is_open() )
+      {
+        return;
+      }
 
-    if ( !m_ofs.is_open() )
+      outputScopedMessage ( "<-" );
+    }
+    else if ( m_isStreamed )
     {
-      return;
+      getOutStream() << std::endl;
     }
+  }
 
-    outputScopedMessage ( "<-" );
+  static std::string const& getOutFileName()
+  {
+    return m_outFileName;
+  }
+
+  std::ostream& getOutStream()
+  {
+    return m_ofs;
   }
 
 private:
   std::string printTimestamp()
   {
-    if ( ! ( m_format & format::timestamp ) )
+    if ( ! ( m_format & static_cast<std::underlying_type<format>::type> ( format::timestamp ) ) )
     {
       return {};
     }
@@ -97,19 +120,20 @@ private:
 
   std::string printDisposition ( size_t lineNo, std::string const& functName )
   {
-    if ( ! ( m_format & ( format::function | format::line ) ) )
+    if ( ! ( m_format & ( static_cast<std::underlying_type<format>::type> ( format::function )
+                          | static_cast<std::underlying_type<format>::type> ( format::line ) ) ) )
     {
       return {};
     }
 
     std::stringstream ss;
 
-    if ( m_format & format::function )
+    if ( m_format & static_cast<std::underlying_type<format>::type> ( format::function ) )
     {
       ss << functName;
     }
 
-    if ( m_format & format::line )
+    if ( m_format & static_cast<std::underlying_type<format>::type> ( format::line ) )
     {
       ss << ":" << lineNo << " ";
     }
@@ -125,11 +149,12 @@ private:
 
 private:
   bool m_isScoped = false;
+  bool m_isStreamed = false;
   std::ofstream m_ofs;
   size_t m_lineNo{};
   std::string m_functName{};
-  static constexpr std::string const& m_outFileName = g_outFileName;
   static constexpr uint8_t m_format = g_defaultFormat;
+  static constexpr std::string const& m_outFileName = g_outFileName;
 };
 
 } // namespace lggm
@@ -137,12 +162,14 @@ private:
 #define LGGM_CAT(a,b) LGGM_CAT2(a, b)
 #define LGGM_CAT2(a,b) a ## b
 
-#define LS() lggm::lggm LGGM_CAT(a,__LINE__) ( __LINE__, __PRETTY_FUNCTION__ )
+#define LS() lggm::lggm().doScope ( __LINE__, __PRETTY_FUNCTION__ )
 
-#define LT() do { lggm::lggm LGGM_CAT(a,__LINE__)( __LINE__, __PRETTY_FUNCTION__ , "trace"); } while(0);
+#define LT() lggm::lggm().doMessage ( __LINE__, __PRETTY_FUNCTION__ , "trace")
 
-#define LM(m) do { lggm::lggm LGGM_CAT(a,__LINE__)( __LINE__, __PRETTY_FUNCTION__ , m ); } while(0);
+#define LM(m) lggm::lggm().doMessage ( __LINE__, __PRETTY_FUNCTION__ , m )
 
-#define L(v) do { lggm::lggm LGGM_CAT(a,__LINE__)( __LINE__, __PRETTY_FUNCTION__ , #v , v ); } while(0);
+#define L(v) lggm::lggm().doNameValue ( __LINE__, __PRETTY_FUNCTION__ , #v , v )
+
+#define LSS(v) lggm::lggm().doStream ( __LINE__, __PRETTY_FUNCTION__ ) << v
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include "lggm/lggm.h"
 
 #include <cstdint>
+#include <cassert>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 
 int main ( int argc, char** argv )
 {
   std::cout << __PRETTY_FUNCTION__ << " ->" << std::endl;
+
+  {
+    std::ofstream logfile ( lggm::lggm::getOutFileName(), std::ios::out | std::ios::trunc );
+    assert ( logfile.is_open() );
+  }
 
   LS();
   LT();
@@ -17,6 +24,33 @@ int main ( int argc, char** argv )
   L ( s );
   LM ( "a" );
   LT();
+  LSS ( "this is a int: " << a << " this is a string: " << s );
+  LT();
+
+  {
+    std::stringstream filecontents;
+
+    {
+      std::ifstream logfile ( lggm::lggm::getOutFileName(), std::ios::in );
+      assert ( logfile.is_open() );
+
+      do
+      {
+        std::string s;
+        std::getline ( logfile, s );
+
+        if ( logfile.fail() )
+        {
+          break;
+        }
+
+        filecontents << s << std::endl;
+      }
+      while ( logfile.good() );
+    }
+
+    std::cout << filecontents.str() <<  std::endl;
+  }
 
   std::cout << __PRETTY_FUNCTION__ << " <-" << std::endl;
 
