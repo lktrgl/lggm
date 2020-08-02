bea37689394a20c9df1a48d0afc7da84c0f161b6
boil <boil@localhost.localdomain>
2020-08-02 12:29:12 +0300

    dbgprn items can be turned off individually

--- include/lggm/impl/dbgprn_macro_impl.h
+++ include/lggm/impl/dbgprn_macro_impl.h
@@ -4,20 +4,37 @@
 #if defined(DBGPRN_ENABLED)
 
   #define LGGM_TRACE() do{ lggmDbg(__FUNCTION__, __LINE__, "trace"); }while(0);
+  #define LGGM_TRACE_C(on) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, "trace");} }while(0);
+
   #define LGGM_CALL_IN() do{ lggmDbg(__FUNCTION__, __LINE__, "->"); }while(0);
+  #define LGGM_CALL_IN_C(on) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, "->");} }while(0);
+
   #define LGGM_CALL_OUT() do{ lggmDbg(__FUNCTION__, __LINE__, "<-"); }while(0);
+  #define LGGM_CALL_OUT_C(on) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, "<-");} }while(0);
+
   #define LGGM_PRINT_MSG(txt) do{ lggmDbg(__FUNCTION__, __LINE__, txt); }while(0);
+  #define LGGM_PRINT_MSG_C(on,txt) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, txt);} }while(0);
+
   #define LGGM_PRINT_INT(var) do{ lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetStrInt(#var, var)); }while(0);
+  #define LGGM_PRINT_INT_C(on,var) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetStrInt(#var, var));} }while(0);
+
   #define LGGM_PRINT_STR(var) do{ lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetStrStr(#var, var)); }while(0);
+  #define LGGM_PRINT_STR_C(on,var) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetStrStr(#var, var));} }while(0);
 
 #else
 
   #define LGGM_TRACE()
+  #define LGGM_TRACE_C(on)
   #define LGGM_CALL_IN()
+  #define LGGM_CALL_IN_C(on)
   #define LGGM_CALL_OUT()
+  #define LGGM_CALL_OUT_C(on)
   #define LGGM_PRINT_MSG(txt)
+  #define LGGM_PRINT_MSG_C(on,txt)
   #define LGGM_PRINT_INT(var)
+  #define LGGM_PRINT_INT_C(on,var)
   #define LGGM_PRINT_STR(var)
+  #define LGGM_PRINT_STR_C(on,var)
 
 #endif
 
