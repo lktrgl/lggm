4923b26f4a53bd1a92ac526be49437fb5f765154
lktrgl <32882989+lktrgl@users.noreply.github.com>
2021-05-21 00:17:55 +0300

    improved ptr arg type casting for hex

--- include/lggm/impl/dbgprn_macro_impl.h
+++ include/lggm/impl/dbgprn_macro_impl.h
@@ -21,8 +21,8 @@
   #define LGGM_PRINT_STR(var) do{ lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetStrStr(#var, var)); }while(0);
   #define LGGM_PRINT_STR_C(on,var) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetStrStr(#var, var));} }while(0);
 
-  #define LGGM_PRINT_HEX(ptr,len) do{ lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetHexStr(#ptr, ptr,len)); }while(0);
-  #define LGGM_PRINT_HEX_C(on,ptr,len) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetHexStr(#ptr, ptr,len));} }while(0);
+  #define LGGM_PRINT_HEX(ptr,len) do{ lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetHexStr(#ptr, ((const char*)(ptr)), len)); }while(0);
+  #define LGGM_PRINT_HEX_C(on,ptr,len) do{ if(on){lggmDbg(__FUNCTION__, __LINE__, lggmDbgGetHexStr(#ptr, ((const char*)(ptr)), len));} }while(0);
 
 #else
 
